367f46a94f686ff5afc7975ae399c0713a868b40
Miguel Dias <miguel-c-dias@telecom.pt>
2016-02-17 18:14:04 +0000

    Fixed Logstash outputs

--- hot-generator/main.rb
+++ hot-generator/main.rb
@@ -21,6 +21,7 @@ ENV['RACK_ENV'] ||= 'development'
 require 'sinatra'
 require 'sinatra/config_file'
 require 'yaml'
+require 'logstash-logger'
 
 # Require the bundler gem and then call Bundler.require to load in all gems
 # listed in Gemfile.
@@ -34,18 +35,29 @@ require_relative 'models/init'
 configure do
 	# Configure logging
 	enable :logging
+	Dir.mkdir("#{settings.root}/log") unless File.exists?("#{settings.root}/log")
 	log_file = File.new("#{settings.root}/log/#{settings.environment}.log", "a+")
 	log_file.sync = true
-	use Rack::CommonLogger, log_file
 end
 
 before do
+	logger = LogStashLogger.new(
+			type: :multi_logger,
+			outputs: [
+					{ type: :stdout, formatter: ::Logger::Formatter },
+					{ host: settings.logstash_host, port: settings.logstash_port }
+			])
+	LogStashLogger.configure do |config|
+		config.customize_event do |event|
+			event["module"] = settings.servicename
+		end
+	end
 	logger.level = Logger::DEBUG
+	env['rack.logger'] = logger
 end
 
 class OrchestratorHotGenerator < Sinatra::Application
 	register Sinatra::ConfigFile
 	# Load configurations
 	config_file 'config/config.yml'
-	#use Rack::CommonLogger, LogStashLogger.new(host: settings.logstash_host, port: settings.logstash_port)
 end
