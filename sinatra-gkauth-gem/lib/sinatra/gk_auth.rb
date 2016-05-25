575a9edb70e24a8bb9f50dbc554be7087daf0708
Josep Batallé <jbatalle3@gmail.com>
2016-05-25 17:24:03 +0200

    Updated GKAuth, included registration/subcription of modules

--- sinatra-gkauth-gem/lib/sinatra/gk_auth.rb
+++ sinatra-gkauth-gem/lib/sinatra/gk_auth.rb
@@ -1,8 +1,22 @@
 require 'sinatra/base'
+require 'json'
 
 module Sinatra
   module Gk_Auth
     module Helpers
+
+      def initialize
+        puts "Initializing gem GK..."
+        begin
+          RestClient.get settings.manager + '/configs/services/publish/' + settings.servicename, :content_type => :json
+        rescue => e
+          puts "error"
+          puts e
+          #halt 400, {'Content-Type' => 'text/plain'}, e.response
+        end
+
+        return
+      end
 
       def authorized?
 
@@ -25,9 +39,17 @@ module Sinatra
         end
       end
 
+      # Checks if a JSON message is valid
+      #
+      # @param [JSON] message some JSON message
+      # @return [Hash, nil] if the parsed message is a valid JSON
+      # @return [Hash, String] if the parsed message is an invalid JSON
       def parse_json(message)
         begin
-          parsed_message = JSON.parse(message) # parse json message
+          parsed_message = ::JSON.parse(message) # parse json message
+        rescue => e
+          puts "Error"
+          puts e
         rescue JSON::ParserError => e
           return message, e.to_s + "\n"
         end
@@ -42,13 +64,23 @@ module Sinatra
         end
       end
 
+      def updateConfigValues(key, value)
+          settings[key] = value
+      end
+
     end
 
     def self.registered(app)
 
       app.helpers Gk_Auth::Helpers
 
+      #Gk_Auth::Helpers.initializer()
+
+
       app.before do
+
+        env['rack.logger'] = settings.logger
+
         if request.path_info == '/gk_credentials'
           return
         end
@@ -71,6 +103,26 @@ module Sinatra
 
         updateValues("gk", credentials['gk_url'])
         updateValues("service_key", credentials['service_key'])
+
+        return 200
+      end
+
+      app.post '/gk_dependencies' do
+
+        return 415 unless request.content_type == 'application/json'
+
+        services, errors = parse_json(request.body.read)
+        return 400, errors.to_json if errors
+
+        puts "Services publishing..."
+        if settings.dependencies.nil?
+          return 200
+        end
+        services.each do |service|
+          if !settings.dependencies.detect{ |sv| sv == service['name'] }.nil?
+            app.set service['name'], service['host']+":"+service['port']
+          end
+        end
 
         return 200
       end
