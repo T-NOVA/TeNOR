bfd82fb91bd5a5a9cadd3b7f1560d9ec609200aa
Josep Batallé <jbatalle3@gmail.com>
2016-07-06 14:24:44 +0200

    VNF Provisioning, adding loop for try to delete a vnf stack

--- vnf-provisioning/routes/vnf.rb
+++ vnf-provisioning/routes/vnf.rb
@@ -198,16 +198,46 @@ class Provisioning < VnfProvisioning
     end
 
     # Requests the VIM to delete the stack
-    begin
-      response = RestClient.delete vnfr.stack_url, 'X-Auth-Token' => auth_token, :accept => :json
-    rescue Errno::ECONNREFUSED
-      halt 500, 'VIM unreachable'
-    rescue RestClient::ResourceNotFound
-      puts "Already removed from the VIM."
-    rescue => e
-      logger.error e.response
-      halt e.response.code, e.response.body
+
+    ##############################################
+    deleteStack(vnfr.stack_url, auth_token)
+
+    status = "DELETING"
+    count = 0
+    while (status != "DELETE_COMPLETE" && status != "DELETE_FAILED")
+      sleep(5)
+      begin
+        response = RestClient.get vnfr.stack_url, 'X-Auth-Token' => auth_token, :content_type => :json, :accept => :json
+        stack_info, error = parse_json(response)
+        status = stack_info['stack']['stack_status']
+      rescue Errno::ECONNREFUSED
+        error = {"info" => "VIM unrechable."}
+        return
+      rescue RestClient::ResourceNotFound
+        logger.info "Stack already removed."
+        status = "DELETE_COMPLETE"
+      rescue => e
+        puts "If no exists means that is deleted correctly"
+        status = "DELETE_COMPLETE"
+        logger.error e
+        logger.error e.response
+      end
+
+      logger.debug "Try: " + count.to_s + ", status: " + status.to_s
+      if (status == "DELETE_FAILED")
+        deleteStack(stack_url, tenant_token)
+        status = "DELETING"
+      end
+      count = count +1
+
+      if count > 10
+        logger.error "Stack can not be removed"
+        raise 400, "Stack can not be removed"
+      end
+      break if count > 20 #to remove
     end
+    ##############################################
+
     logger.debug 'VIM response to destroy: ' + response.to_json
 
     # Delete the VNFR from mAPI
