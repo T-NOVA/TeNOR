0364b93f75825655d073d5c72f970a58d4b16305
Josep Batallé <jbatalle3@gmail.com>
2016-08-01 14:20:07 +0200

    Updated dependecies script

--- dependencies/install_dependencies.sh
+++ dependencies/install_dependencies.sh
@@ -42,45 +42,60 @@ function echo_if {
   fi
 }
 
-
 echo "Checking if mongodb is installed"
-
-#mongod --version > /dev/null 2>&1
-#MONGO_IS_INSTALLED=$?
-#if [ $MONGO_IS_INSTALLED -eq 0 ]; then
-#    echo ">>> MongoDB already installed"
-#    #service mongod restart
-#else
-#    echo "Installing mongodb..."
-#    ./install_mongodb.sh
-#    #bash -c "$(curl -fsSL https://raw.githubusercontent.com/steveneaston/Vaprobash/master/scripts/mongodb.sh)" bash $1 $2
-#fi
+mongod --version > /dev/null 2>&1
+MONGO_IS_INSTALLED=$?
+if [ $MONGO_IS_INSTALLED -eq 0 ]; then
+    echo ">>> MongoDB already installed"
+    #service mongod restart
+else
+    echo "Mongodb is not installed... Installing..."
+    ./install_mongodb.sh
+    #bash -c "$(curl -fsSL https://raw.githubusercontent.com/steveneaston/Vaprobash/master/scripts/mongodb.sh)" bash $1 $2
+fi
 
 echo "Checking if gatekeeper is installed"
 if [ -f ~/go/bin/auth-utils ]; then
-  echo "Gatekeeper is installed."
+  echo ">>> Gatekeeper already installed."
   if [ ! -f ~/gatekeeper.cfg ]; then
     cp go/src/github.com/piyush82/auth-utils/gatekeeper.cfg ~
   fi
 else
-    echo "Gatekeeper is not installed"
-    #./install_gatekeeper.sh
+    echo "Gatekeeper is not installed. Installing..."
+    sudo apt-get install gcc -y
+    ./install_gatekeeper.sh
 fi
 
 echo "Checking if ruby is installed"
-
-ruby_version=`ruby -e "print(RUBY_VERSION < '2.3.0' ? '1' : '0' )"`
-if [ $ruby_version -eq 1 ]; then
-    echo "Ruby version: " $RUBY_VERSION
-    echo "Please, install a ruby version higher or equal to 2.3.0"
+. ~/.rvm/scripts/rvm
+ruby --version > /dev/null 2>&1
+RUBY_IS_INSTALLED=$?
+if [ $RUBY_IS_INSTALLED -eq 0 ]; then
+    ruby_version=`ruby -e "print(RUBY_VERSION < '2.2.0' ? '1' : '0' )"`
+    if [ $ruby_version -eq 1 ]; then
+        echo "Ruby version: " $RUBY_VERSION
+        echo "Please, install a ruby version higher or equal to 2.2.0"
+    else
+        echo ">>> Ruby is already installed"
+    fi
 else
-    echo "Ruby version: " $RUBY_VERSION
+    echo "Ruby is not installed."
+    ./install_ruby.sh
+    . ~/.rvm/scripts/rvm
 fi
 
-$RUBY_VERSION
+npm --version > /dev/null 2>&1
+NPM_IS_INSTALLED=$?
+if [ $NPM_IS_INSTALLED -eq 0 ]; then
+    echo ">>> NPM is already installed"
+else
+    echo "NPM is not installed."
+    ./install_npm.sh
+fi
 
-echo "Checking if npm is installed"
-echo "node          $(echo_if $(program_is_installed node))"
-echo "npm          $(echo_if $(program_is_installed npm))"
-echo "ruby          $(echo_if $(program_is_installed ruby))"
-echo "bundler          $(echo_if $(program_is_installed bundler))"
+echo "Checking if dependencies are installed"
+echo "mongod          $(echo_if $(program_is_installed mongo))"
+echo "ruby            $(echo_if $(program_is_installed ruby))"
+echo "bundler         $(echo_if $(program_is_installed bundler))"
+echo "node            $(echo_if $(program_is_installed node))"
+echo "npm             $(echo_if $(program_is_installed npm))"
