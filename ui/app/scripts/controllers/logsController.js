988d1e3da78b64f7e24967593f519bfe102aa0b9
Josep Batallé <jbatalle3@gmail.com>
2016-09-27 16:19:12 +0200

    UI Logs view updated

--- ui/app/scripts/controllers/logsController.js
+++ ui/app/scripts/controllers/logsController.js
@@ -5,13 +5,18 @@ angular.module('tNovaApp')
 
         $scope.log = [];
         $scope.severities = ["ERROR", "DEBUG", "INFO"];
+        $scope.modules = ["ns_manager", "ns_catalogue", "ns_provisioning", "ns_monitoring", "vnf_manager", "vnf_catalogue", "vnf_provisioner", "vnf_monitoring", "hot_generator"];
         $scope.days = [];
         $scope.selectedDay = "";
         $scope.selectedSeverity = "ERROR";
+        $scope.selectedModule = "";
         var url = "elastic/*/_stats/store"
         tenorService.get(url).then(function (data) {
             if (!data) return;
             var indices = Object.keys(data.indices);
+            indices = _.sortBy(indices, function (name) {
+                return name
+            }).reverse();
             console.log(indices);
             angular.forEach(indices, function (index) {
                 $scope.days.push(index);
@@ -19,16 +24,25 @@ angular.module('tNovaApp')
         });
 
         $scope.getLogs = function (day) {
-            var url = "elastic/" + $scope.selectedDay + "/_search?pretty=1&q=*&size=200&q=severity:" + $scope.selectedSeverity;
+            $scope.selectedDay = day;
+            var url = "elastic/" + $scope.selectedDay + "/_search?pretty=1&q=*&size=2000&q=severity:" + $scope.selectedSeverity;
             tenorService.get(url).then(function (data) {
                 if (!data) return;
                 $scope.log = data.hits.hits;
             });
         }
 
-        $scope.selectSeverity = function (severity) {
-            console.log($scope.selectedSeverity);
-            var url = "elastic/" + $scope.selectedDay + "/_search?pretty=1&q=*&size=200&q=severity:" + $scope.selectedSeverity;
+        $scope.getBySeverity = function (severity) {
+            var url = "elastic/" + $scope.selectedDay + "/_search?pretty=1&q=*&size=2000&q=severity:" + $scope.selectedSeverity;
+            tenorService.get(url).then(function (data) {
+                if (!data) return;
+                $scope.log = data.hits.hits;
+            });
+        };
+
+        $scope.getByModule = function (module) {
+            var url = "elastic/" + $scope.selectedDay + "/_search?pretty=1&size=2000&q=module:" + $scope.selectedModule;
+            console.log(url)
             tenorService.get(url).then(function (data) {
                 if (!data) return;
                 $scope.log = data.hits.hits;
