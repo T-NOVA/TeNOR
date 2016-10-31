af354686012e90f84c5a67103c4e2be45c8d80ac
Josep Batallé <jbatalle3@gmail.com>
2016-10-31 16:53:07 +0100

    Updated HOT Generator specs

--- hot-generator/spec/network_spec.rb
+++ hot-generator/spec/network_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe HotGenerator do
       nsd ='{
 					"nsd":
 					{"id":"56df37d5e4b01f97669827ad","name":"PXaaS 333","vendor":"3", "vld":{"number_of_endpoints":0,"virtual_links":[{"vld_id":"vld0","alias":"management","root_requirements":"Unlimeted","leaf_requirement":"Unlimeted","qos":{"average":"","peak":"","burst":""},"connections":["VNF#1002:ext_management"],"connectivity_type":"E-LAN","external_access":false,"merge":true,"sla_ref_id":"sla0"},{"vld_id":"vld1","alias":"ingress","root_requirements":"Unlimeted","leaf_requirement":"Unlimeted","qos":{"average":"","peak":"","burst":""},"connections":["VNF#1002:ext_data_in"],"connectivity_type":"E-LINE","external_access":true,"merge":false,"sla_ref_id":"sla0"},{"vld_id":"vld2","alias":"engress","root_requirements":"Unlimeted","leaf_requirement":"Unlimeted","qos":{"average":"","peak":"","burst":""},"connections":["VNF#1002:ext_data_out"],"connectivity_type":"E-LINE","external_access":true,"merge":false,"sla_ref_id":"sla0"}]}},
-					"public_net_id": "network_id", "dns_server": "10.10.1.1"
+					"public_net_id": "network_id", "dns_server": "10.10.1.1", "nsr_id": "nsr_id"
 			}'
 
       let(:response) { post '/networkhot/flavor0', nsd, rack_env={'CONTENT_TYPE' => 'application/json'} }
@@ -36,7 +36,7 @@ RSpec.describe HotGenerator do
       end
 
       it 'response body should be equal' do
-        valid_response = '{"heat_template_version":"2014-10-16","description":"PXaaS 333","resources":{"56df37d5e4b01f97669827ad_0":{"type":"OS::Neutron::Router","properties":{"external_gateway_info":{"network":"network_id"},"name":"Tenor router"}}},"outputs":{}}'
+        valid_response = '{"heat_template_version":"2015-04-30","description":"PXaaS 333","parameters":{},"resources":{"56df37d5e4b01f97669827ad_0":{"type":"OS::Neutron::Router","properties":{"external_gateway_info":{"network":"network_id"},"name":"Tenor_nsr_id"}}},"outputs":{}}'
         expect(JSON.parse response.body).to eq(JSON.parse valid_response)
       end
     end
