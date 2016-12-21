91c9bb8bbb303e5cdb7413ee86929d81d461ceb9
Josep Batallé <jbatalle3@gmail.com>
2016-12-21 08:55:28 +0100

    UI fix scroll in modals

--- ui/app/scripts/directives/modalScroll.js
+++ ui/app/scripts/directives/modalScroll.js
@@ -10,6 +10,27 @@ angular.module('tNovaApp')
                 }, function (newValue, oldValue) {
                     var windowElement = angular.element($window);
                     var scrollHeight = element[0].scrollHeight;
+                    if (scrollHeight + 180 > windowElement.height()) {
+                        console.log("Set bigger");
+                        element.css('height', windowElement.height() - 220);
+                    } else {
+                        console.log("Set small");
+                        if(scrollHeight == 22) element.css('height', windowElement.height() - 200);
+                        else element.css('height', scrollHeight);
+                    }
+                });
+            }
+        }
+    }).directive('modalscrollupload', function ($window) {
+        return {
+            priority: 0,
+            link: function (scope, element) {
+                var i = 0;
+                scope.$watch(function () {
+                    return $(element).height();
+                }, function (newValue, oldValue) {
+                    var windowElement = angular.element($window);
+                    var scrollHeight = element[0].scrollHeight;
                     if (scrollHeight + 180 < windowElement.height()) {
                         console.log("Set bigger");
                         element.css('height', windowElement.height() - 220);
